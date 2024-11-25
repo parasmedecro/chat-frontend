ceb4960c61bea7674d49ab6626aa41f0fbfac697
parasmedecro <paras.jain@medecro.ai>
2024-11-25 16:37:31 +0530

    new commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-white text-5xl h-screen text-center p-52">
+      <h1 >
+      Please click the Chat button to start chating 
+      </h1>
+    </div>
+  );
+}
